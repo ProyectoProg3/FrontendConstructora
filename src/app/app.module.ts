b9fe9b184224090c043068d83ce6334d0d2b325f
juandavidoh3 <72204602+juandavidoh3@users.noreply.github.com>
2021-07-13 20:35:39 -0500

    Creación componente público

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionSuperiorComponent } from './publico/paginaMaestra/barra-navegacion-superior/barra-navegacion-superior.component';
+import { BarraLateralComponent } from './publico/paginaMaestra/barra-lateral/barra-lateral.component';
+import { PieDePaginaComponent } from './publico/paginaMaestra/pie-de-pagina/pie-de-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionSuperiorComponent,
+    BarraLateralComponent,
+    PieDePaginaComponent
   ],
   imports: [
     BrowserModule,
