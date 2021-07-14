643b1b5b7563b1e3d3cf5e185cb8d1319faafd56
juandavidoh3 <72204602+juandavidoh3@users.noreply.github.com>
2021-07-13 19:06:02 -0500

    Módulos: Seguridad, parametrización y Usuario

---
+++ src/app/modulos/parametrizacion/parametrizacion.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ParametrizacionRoutingModule } from './parametrizacion-routing.module';
+import { ListarCiudadComponent } from './ciudad/listar-ciudad/listar-ciudad.component';
+import { CrearCiudadComponent } from './ciudad/crear-ciudad/crear-ciudad.component';
+import { EditarCiudadComponent } from './ciudad/editar-ciudad/editar-ciudad.component';
+import { EliminarCiudadComponent } from './ciudad/eliminar-ciudad/eliminar-ciudad.component';
+import { ListarPaisComponent } from './pais/listar-pais/listar-pais.component';
+import { CrearPaisComponent } from './pais/crear-pais/crear-pais.component';
+import { EditarPaisComponent } from './pais/editar-pais/editar-pais.component';
+import { EliminarPaisComponent } from './pais/eliminar-pais/eliminar-pais.component';
+import { ListarProyectoComponent } from './proyecto/listar-proyecto/listar-proyecto.component';
+import { CrearProyectoComponent } from './proyecto/crear-proyecto/crear-proyecto.component';
+import { EditarProyectoComponent } from './proyecto/editar-proyecto/editar-proyecto.component';
+import { EliminarProyectoComponent } from './proyecto/eliminar-proyecto/eliminar-proyecto.component';
+import { ListarInmuebleComponent } from './inmueble/listar-inmueble/listar-inmueble.component';
+import { CrearInmuebleComponent } from './inmueble/crear-inmueble/crear-inmueble.component';
+import { EditarInmuebleComponent } from './inmueble/editar-inmueble/editar-inmueble.component';
+import { EliminarInmuebleComponent } from './inmueble/eliminar-inmueble/eliminar-inmueble.component';
+import { ListarBloqueComponent } from './bloque/listar-bloque/listar-bloque.component';
+import { CrearBloqueComponent } from './bloque/crear-bloque/crear-bloque.component';
+import { EditarBloqueComponent } from './bloque/editar-bloque/editar-bloque.component';
+import { EliminarBloqueComponent } from './bloque/eliminar-bloque/eliminar-bloque.component';
+import { ListarClienteComponent } from './cliente/listar-cliente/listar-cliente.component';
+import { CrearClienteComponent } from './cliente/crear-cliente/crear-cliente.component';
+import { EditarClienteComponent } from './cliente/editar-cliente/editar-cliente.component';
+import { EliminarClienteComponent } from './cliente/eliminar-cliente/eliminar-cliente.component';
+
+
+@NgModule({
+  declarations: [
+    ListarCiudadComponent,
+    CrearCiudadComponent,
+    EditarCiudadComponent,
+    EliminarCiudadComponent,
+    ListarPaisComponent,
+    CrearPaisComponent,
+    EditarPaisComponent,
+    EliminarPaisComponent,
+    ListarProyectoComponent,
+    CrearProyectoComponent,
+    EditarProyectoComponent,
+    EliminarProyectoComponent,
+    ListarInmuebleComponent,
+    CrearInmuebleComponent,
+    EditarInmuebleComponent,
+    EliminarInmuebleComponent,
+    ListarBloqueComponent,
+    CrearBloqueComponent,
+    EditarBloqueComponent,
+    EliminarBloqueComponent,
+    ListarClienteComponent,
+    CrearClienteComponent,
+    EditarClienteComponent,
+    EliminarClienteComponent
+  ],
+  imports: [
+    CommonModule,
+    ParametrizacionRoutingModule
+  ]
+})
+export class ParametrizacionModule { }
